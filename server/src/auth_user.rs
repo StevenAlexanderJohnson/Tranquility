60af942c28f5fc6625bae10d0a20a6dc91763f64
StevenAlexanderJohnson <56038965+StevenAlexanderJohnson@users.noreply.github.com>
2024-08-24 11:20:23 -0400

    changing id to objectId in auth_user

--- server/src/auth_user.rs
+++ server/src/auth_user.rs
@@ -1,9 +1,10 @@
+use mongodb::bson::oid::ObjectId;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct AuthUser {
     #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
-    pub id: Option<String>,
+    pub id: Option<ObjectId>,
     pub username: String,
     pub password: String,
     pub email: Option<String>,
