7548e05a73abfe118b79f48ca518a4ad2f7b81b7
StevenAlexanderJohnson <56038965+StevenAlexanderJohnson@users.noreply.github.com>
2024-08-25 22:52:37 -0400

    one last thing, putting auth_repository insert back

--- data_access/src/auth/auth_repository.rs
+++ data_access/src/auth/auth_repository.rs
@@ -15,22 +15,13 @@ impl AuthRepository {
         &self,
         auth_user: &AuthUser,
     ) -> Result<AuthUser, Box<dyn std::error::Error>> {
-        match sqlx::query_as::<_, AuthUser>(
+        sqlx::query_as::<_, AuthUser>(
             "INSERT INTO auth (username, password, email) VALUES ($1, $2, $3) RETURNING id, username, email, refresh_token;"
         )
         .bind(auth_user.username.to_string())
         .bind(auth_user.password.as_ref().expect("Password was not provided.").to_string())
         .bind(auth_user.email.as_ref().expect("Email has not been provided.").to_string())
-        .fetch_one(&self.pool).await {
-            Ok(row) => {
-                println!("{:?}", row);
-                Ok(row)
-            },
-            Err(e) => {
-                println!("Something: {:?}", e);
-                Err("Unable to insert".into())
-            }
-        }
+        .fetch_one(&self.pool).await.map_err(|e| e.into())
     }
 
     pub async fn find(
