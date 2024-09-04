89a4e502714ce37b170b8f04d8589eabd169387b
StevenAlexanderJohnson <56038965+StevenAlexanderJohnson@users.noreply.github.com>
2024-09-04 17:38:45 -0400

    updating websocket endpoint to handle how I think we should handle messages over the connection

--- src/endpoints/websocket.rs
+++ src/endpoints/websocket.rs
@@ -1,5 +1,25 @@
 use actix_web::{get, rt, web, Error, HttpRequest, HttpResponse};
 use actix_ws::AggregatedMessage;
+use data_access::Guild;
+use models::{MessageData, WebSocketMessage};
+
+mod models {
+    use data_access::{Channel, Guild};
+    use serde;
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub enum MessageData {
+        Channel(Channel),
+        Guild(Guild),
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct WebSocketMessage {
+        pub message_type: String,
+        pub data: Option<MessageData>,
+    }
+}
 
 #[get("/")]
 pub async fn echo(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
@@ -15,10 +35,7 @@ pub async fn echo(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse
         while let Some(Ok(msg)) = stream.recv().await {
             let mut session = session.clone();
             match msg {
-                AggregatedMessage::Text(text) => {
-                    println!("Text received: {}", text);
-                    session.text(text).await.unwrap()
-                }
+                AggregatedMessage::Text(text) => handle_json_request(&text, &mut session).await,
                 AggregatedMessage::Ping(msg) => {
                     println!("Ping received");
                     session.pong(&msg).await.unwrap()
@@ -33,4 +50,23 @@ pub async fn echo(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse
     });
 
     Ok(res)
+}
+
+async fn handle_json_request(message: &str, session: &mut actix_ws::Session) {
+    let message: WebSocketMessage =
+        serde_json::from_str(message).expect("Unable to deserialize message");
+
+    match message.data {
+        Some(MessageData::Channel(t)) => println!("{:?}", t),
+        Some(MessageData::Guild(t)) => println!("{:?}", t),
+        None => println!("No Data"),
+    }
+
+    let response = WebSocketMessage {
+        message_type: "Ack".into(),
+        data: Some(MessageData::Guild(Guild {
+            ..Default::default()
+        }))
+    };
+    session.text(serde_json::to_string(&response).expect("Unable to stringify message")).await.expect("unable to send message to client");
 }
