ec40c03778f79a3f566222ef63ac66780a4be1a9
StevenAlexanderJohnson <56038965+StevenAlexanderJohnson@users.noreply.github.com>
2024-10-02 21:58:55 -0400

    creating table for attachments and updating script for create message sql script

--- database_scripts/07-create_message_table.sql
+++ database_scripts/07-create_message_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE (
+CREATE TABLE message (
     id SERIAL PRIMARY KEY,
     channel_id INTEGER REFERENCES channel(id),
     author_id INTEGER REFERENCES auth(id),
---
+++ database_scripts/08-create_attachment_table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE attachment (
+    id SERIAL PRIMARY KEY,
+    content_url: TEXT
+);
