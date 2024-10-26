8ce71c1c544c58b7c885180ef22269972b25c07e
StevenAlexanderJohnson <56038965+StevenAlexanderJohnson@users.noreply.github.com>
2024-10-26 16:29:13 -0400

    making all models derive debug

--- data_models/src/guilds.rs
+++ data_models/src/guilds.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct CreateGuildRequest {
     pub name: String,
     pub description: String,
--- data_models/src/messages.rs
+++ data_models/src/messages.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct CreateMessageRequest {
     pub channel_id: i32,
     pub content: String,
