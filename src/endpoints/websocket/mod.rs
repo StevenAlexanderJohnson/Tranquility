7c0819afc23a6504deb0c1f77f1d3d61f517bf7a
StevenAlexanderJohnson <56038965+StevenAlexanderJohnson@users.noreply.github.com>
2024-11-03 08:44:16 -0500

    removing uneccessary hello message

--- src/endpoints/websocket/mod.rs
+++ src/endpoints/websocket/mod.rs
@@ -13,6 +13,7 @@ pub async fn gateway(
     path: web::Path<(i32, String)>,
     repository: web::Data<DatabaseConnection>,
 ) -> Result<HttpResponse, Error> {
+    // Handle the handshake
     let (res, session, stream) = match actix_ws::handle(&req, stream) {
         Ok(res) => res,
         Err(e) => {
@@ -21,6 +22,7 @@ pub async fn gateway(
     };
     println!("WebSocket connection initiated");
 
+    // Login with path variables
     let user = match repository.websocket_login(path.0, &path.1).await {
         Ok(user) => 
         {
@@ -44,32 +46,6 @@ pub async fn gateway(
         .max_continuation_size(2_usize.pow(20));
 
     rt::spawn(async move {
-        let hello_message = stream
-            .recv()
-            .await
-            .and_then(|result| result.ok())
-            .and_then(|msg| match msg {
-                AggregatedMessage::Text(addr) => Some(addr),
-                _ => None,
-            });
-        if hello_message.is_none()
-            || match handle_hello_request(&hello_message.unwrap()) {
-                Ok(_) => false,
-                Err(e) => {
-                    eprintln!("Error handling hello request: {:?}", e);
-                    true
-                }
-            }
-        {
-            let _ = session
-                .close(Some(CloseReason {
-                    code: actix_ws::CloseCode::Invalid,
-                    description: Some(String::from("Invalid hello message")),
-                }))
-                .await;
-            return;
-        }
-
         while let Some(Ok(msg)) = stream.recv().await {
             let mut session = session.clone();
             match msg {
@@ -92,25 +68,6 @@ pub async fn gateway(
     });
 
     Ok(res)
-}
-
-fn handle_hello_request(message: &str) -> Result<(), Box<dyn std::error::Error>> {
-    match serde_json::from_str::<WebSocketMessage>(message)?.data {
-        MessageData::Hello(h) => {
-            println!("{:?}", h);
-            Ok(())
-        }
-        MessageData::Channel(_) => Err(Box::from(
-            "Received create channel request but expected hello.",
-        )),
-        MessageData::Guild(_) => Err(Box::from(
-            "Received create guild request but expected hello.",
-        )),
-        MessageData::Message(_) => Err(Box::from(
-            "Received create message request but expected hello.",
-        )),
-        MessageData::Ack(_) => Err(Box::from("Received ack but expected hello.")),
-    }
 }
 
 async fn handle_json_request(
