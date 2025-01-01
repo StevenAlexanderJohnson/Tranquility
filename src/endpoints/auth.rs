f61cdb0fc86c4e15c085af42fe01acec61eb3f78
StevenAlexanderJohnson <56038965+StevenAlexanderJohnson@users.noreply.github.com>
2025-01-01 11:56:04 -0500

    changing refresh token to use the same struct as login for response. removed old cookie code

--- src/endpoints/auth.rs
+++ src/endpoints/auth.rs
@@ -1,13 +1,8 @@
-use actix_web::{
-    cookie::{
-        time::{Duration, OffsetDateTime},
-        Cookie,
-    },
-    post, web, HttpResponse,
-};
+use actix_web::{post, web, HttpResponse};
 use data_access::{AuthUser, DatabaseConnection};
 
 use data_models::{AuthUserResponse, CreateAuthUserRequest, RefreshTokenRequest};
+use log::error;
 
 use crate::password_manager::hash_password;
 use crate::{
@@ -43,16 +38,11 @@ pub async fn login(
                     return HttpResponse::InternalServerError().finish();
                 }
             };
-            let cookie = Cookie::build("auth_token", &jwt)
-                .domain("localhost")
-                .path("/")
-                .expires(OffsetDateTime::now_utc().checked_add(Duration::minutes(10)))
-                .finish();
 
             AuthUserResponse::try_from(user)
                 .map(|mut response| {
                     response.token = jwt.clone();
-                    HttpResponse::Ok().cookie(cookie).json(response)
+                    HttpResponse::Ok().json(response)
                 })
                 .unwrap_or_else(|e| {
                     println!("{:?}", e);
@@ -107,22 +97,29 @@ pub async fn refresh_token(
         Ok(Some(auth_user)) => auth_user,
         Ok(None) => return HttpResponse::Unauthorized().finish(),
         Err(e) => {
-            println!("{:?}", e);
+            error!("Request to collect user information failed: {}", e);
             return HttpResponse::Unauthorized().finish();
         }
     };
+
     let jwt = match generate_token(&auth_user) {
         Ok(jwt) => jwt,
         Err(e) => {
-            println!("Failed to generate token: {}", e);
+            error!("Failed to geenrate token: {}", e);
             return HttpResponse::InternalServerError().finish();
         }
     };
-    let cookie = Cookie::build("auth_token", jwt)
-        .domain("localhost")
-        .path("/")
-        .expires(OffsetDateTime::now_utc().checked_add(Duration::minutes(10)))
-        .finish();
 
-    HttpResponse::Ok().cookie(cookie).json(auth_user)
+    AuthUserResponse::try_from(auth_user)
+        .map(|mut response| {
+            response.token = jwt.clone();
+            HttpResponse::Ok().json(response)
+        })
+        .unwrap_or_else(|e| {
+            println!("{:?}", e);
+            HttpResponse::InternalServerError().finish()
+        })
+    // AuthUserResponse::try_from(user).map(|mut response| )
+
+    // HttpResponse::Ok().json(auth_user)
 }
