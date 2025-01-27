10c597b92fa0d37b96de99dba5c0503cfc594c70
StevenAlexanderJohnson <56038965+StevenAlexanderJohnson@users.noreply.github.com>
2025-01-26 20:44:18 -0500

    Feature/api21 (#25)
    * updating websockets to be handled through DI
    
    * removing println for logs and removed unused message type
    
    * fixed build error
    
    * removing unused function in notification

---
+++ notifications/src/lib.rs
@@ -0,0 +1,159 @@
+use models::WebsocketResponseData;
+use std::collections::HashMap;
+use tokio::{
+    io,
+    sync::{mpsc, oneshot},
+};
+
+pub mod models;
+
+type ConnId = usize;
+
+#[derive(Debug)]
+enum Command {
+    Connect {
+        user_id: ConnId,
+        conn_tx: mpsc::UnboundedSender<WebsocketResponseData>,
+        res_tx: oneshot::Sender<()>,
+    },
+
+    Disconnect {
+        conn: ConnId,
+        res_tx: oneshot::Sender<()>,
+    },
+
+    Message {
+        msg: WebsocketResponseData,
+        res_tx: oneshot::Sender<()>,
+    },
+}
+
+#[derive(Debug)]
+pub struct WebsocketServer {
+    sessions: HashMap<ConnId, mpsc::UnboundedSender<WebsocketResponseData>>,
+    cmd_rx: mpsc::UnboundedReceiver<Command>,
+}
+
+impl WebsocketServer {
+    pub fn new() -> (Self, WebsocketServerHandler) {
+        let (cmd_tx, cmd_rx) = mpsc::unbounded_channel::<Command>();
+
+        (
+            Self {
+                sessions: HashMap::new(),
+                cmd_rx,
+            },
+            WebsocketServerHandler { cmd_tx },
+        )
+    }
+
+    async fn send_system_message(&self, msg: WebsocketResponseData) {
+        for (_, tx) in &self.sessions {
+            let msg = msg.clone();
+            let _ = tx.send(msg).unwrap();
+        }
+    }
+
+    async fn connect(&mut self, user_id: ConnId, tx: mpsc::UnboundedSender<WebsocketResponseData>) {
+        log::info!("Someone joined");
+
+        self.sessions.insert(user_id, tx);
+
+        self.send_system_message(WebsocketResponseData::Ack("Someone has joined".into()))
+            .await;
+    }
+
+    async fn disconnect(&mut self, conn_id: ConnId, tx: oneshot::Sender<()>) {
+        log::info!("Someone disconnected");
+
+        if self.sessions.remove(&conn_id).is_some() {
+            self.send_system_message(WebsocketResponseData::Ack("User Removed".into()))
+                .await;
+        }
+
+        let _ = tx.send(()).unwrap();
+    }
+
+    pub async fn run(mut self) -> io::Result<()> {
+        log::info!("Notification Server is running.");
+        while let Some(cmd) = self.cmd_rx.recv().await {
+            match cmd {
+                Command::Connect {
+                    user_id,
+                    conn_tx,
+                    res_tx,
+                } => {
+                    let conn_id = self.connect(user_id, conn_tx).await;
+                    let _ = res_tx.send(conn_id).unwrap();
+                    // let conn_id = self.sessions.insert(0, conn_tx);
+                }
+                Command::Disconnect { conn, res_tx } => {
+                    self.disconnect(conn, res_tx).await;
+                }
+                Command::Message { msg, res_tx } => {
+                    self.send_system_message(msg).await;
+                    let _ = res_tx.send(()).unwrap();
+                }
+            }
+        }
+        Ok(())
+    }
+}
+
+/// # WebsocketServerHandler
+///
+/// This struct is passed into actix as a Data dependency.
+/// It is initialized when `WebsocketServer` gets initialized.
+///
+/// ## Properties
+///
+/// * `cmd_tx`: This is a channel that is able to send `Command` values to the websocket server.
+#[derive(Debug, Clone)]
+pub struct WebsocketServerHandler {
+    cmd_tx: mpsc::UnboundedSender<Command>,
+}
+
+impl WebsocketServerHandler {
+    pub async fn connect(
+        &self,
+        user_id: ConnId,
+        conn_tx: mpsc::UnboundedSender<WebsocketResponseData>,
+    ) {
+        let (res_tx, res_rx) = oneshot::channel();
+
+        self.cmd_tx
+            .send(Command::Connect {
+                user_id,
+                conn_tx: conn_tx,
+                res_tx: res_tx,
+            })
+            .unwrap();
+
+        res_rx.await.unwrap()
+    }
+
+    pub async fn disconnect(&self, connection_id: ConnId) {
+        let (res_tx, res_rx) = oneshot::channel();
+        self.cmd_tx
+            .send(Command::Disconnect {
+                conn: connection_id,
+                res_tx,
+            })
+            .unwrap();
+
+        res_rx.await.unwrap()
+    }
+
+    pub async fn send_message(&self, msg: WebsocketResponseData) {
+        let (res_tx, res_rx) = oneshot::channel();
+
+        self.cmd_tx
+            .send(Command::Message {
+                msg: msg,
+                res_tx: res_tx,
+            })
+            .unwrap();
+
+        res_rx.await.unwrap();
+    }
+}
--- src/endpoints/websocket/mod.rs
+++ src/endpoints/websocket/mod.rs
@@ -1,12 +1,31 @@
-mod message;
+use std::{
+    sync::Arc,
+    time::{Duration, Instant},
+};
 
 use actix_web::{error::ErrorUnauthorized, get, rt, web, Error, HttpRequest, HttpResponse};
 
-use actix_ws::{AggregatedMessage, CloseReason};
+use actix_ws::{AggregatedMessage, CloseCode, CloseReason};
 use data_access::DatabaseConnection;
-use data_models::{WebSocketMessage, WebsocketMessageData, WebsocketResponseData};
-use log::error;
+use log::{error, info};
 use message::handle_message;
+use notifications::{
+    models::{WebsocketMessage, WebsocketMessageData, WebsocketResponseData},
+    WebsocketServerHandler,
+};
+use tokio::{select, sync::mpsc, time::interval};
+
+mod message;
+
+const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
+const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
+
+#[derive(Debug)]
+enum DisconnectReason {
+    Timeout,
+    Disconnect,
+    Error,
+}
 
 #[get("/{id}/{token}")]
 pub async fn gateway(
@@ -14,6 +33,7 @@ pub async fn gateway(
     stream: web::Payload,
     path: web::Path<(i32, String)>,
     repository: web::Data<DatabaseConnection>,
+    websocket_server: web::Data<WebsocketServerHandler>,
 ) -> Result<HttpResponse, Error> {
     // Handle the handshake
     let (res, session, stream) = match actix_ws::handle(&req, stream) {
@@ -22,13 +42,13 @@ pub async fn gateway(
             return Err(e);
         }
     };
-    println!("WebSocket connection initiated");
+    info!("WebSocket connection initiated");
 
     // Login with path variables
     let user = match repository.websocket_login(path.0, &path.1).await {
         Ok(user) => user,
         Err(e) => {
-            println!("Error while logging in in websocket: {:?}", e);
+            error!("Error while logging in in websocket: {:?}", e);
             let _ = session
                 .close(Some(CloseReason {
                     code: actix_ws::CloseCode::Invalid,
@@ -40,67 +60,143 @@ pub async fn gateway(
     };
 
     let mut stream = stream
+        .max_frame_size(128 * 1024)
         .aggregate_continuations()
         // aggregate continuation frames up to 1MiB
         .max_continuation_size(2_usize.pow(20));
 
     rt::spawn(async move {
-        while let Some(Ok(msg)) = stream.recv().await {
-            let mut session = session.clone();
-            match msg {
-                AggregatedMessage::Text(text) => {
-                    handle_json_request(&text, user.id, &mut session, &repository).await
+        let websocket_server = websocket_server.clone().into_inner();
+        let (conn_tx, mut conn_rx) = mpsc::unbounded_channel();
+
+        let mut last_heartbeat = Instant::now();
+        let mut interval = interval(HEARTBEAT_INTERVAL);
+
+        websocket_server.connect(user.id as usize, conn_tx).await;
+
+        let close_reason: DisconnectReason = loop {
+            select! {
+                // Handles messages coming in over the websocket from the user.
+                stream_message = stream.recv() => {
+                    match stream_message {
+                        Some(Ok(msg)) => {
+                            let mut session = session.clone();
+                            match msg {
+                                AggregatedMessage::Text(text) => {
+                                    let message: WebsocketMessage =
+                                        serde_json::from_str(&text).expect("Unable to deserialize message");
+
+                                    if let WebsocketMessage::Ping(_) = message {
+                                        last_heartbeat = Instant::now();
+                                        session.pong(b"").await.unwrap();
+                                        continue;
+                                    }
+
+                                    handle_json_request(
+                                        &websocket_server,
+                                        &message,
+                                        user.id,
+                                        &mut session,
+                                        &repository,
+                                    )
+                                    .await
+                                }
+                                AggregatedMessage::Ping(msg) => {
+                                    last_heartbeat = Instant::now();
+                                    info!("Ping received, {}", user.id);
+                                    session.pong(&msg).await.unwrap()
+                                }
+                                AggregatedMessage::Close(msg) => {
+                                    info!("Session Closed");
+                                    session.close(msg).await.unwrap();
+                                    break DisconnectReason::Disconnect;
+                                }
+                                _ => {
+                                    error!("How did you get here")
+                                }
+                            }
+                        },
+                        Some(Err(e)) => {
+                            log::error!("An error occurred while receiving stream message: {} -> {:?}", user.id, e);
+                            break DisconnectReason::Error;
+                        },
+                        None => {
+                            log::error!("None was sent over the stream: {}", user.id);
+                            break DisconnectReason::Error;
+                        }
+                    }
+                },
+                // Handles messages coming from the server to the websocket.
+                msg = conn_rx.recv() => {
+                    let mut session = session.clone();
+                    session
+                        .text(serde_json::to_string(&msg).expect("Unable to stringify message"))
+                        .await
+                    .expect("unable to send message to client");
+                },
+                _ = interval.tick() => {
+                    if Instant::now().duration_since(last_heartbeat) > CLIENT_TIMEOUT {
+                        log::info!("Client {} has not sent a heartbeat in over {CLIENT_TIMEOUT:?}; disconnecting...", user.id);
+                        break DisconnectReason::Timeout;
+                    }
                 }
-                AggregatedMessage::Ping(msg) => {
-                    println!("Ping received");
-                    session.pong(&msg).await.unwrap()
-                }
-                AggregatedMessage::Close(msg) => {
-                    println!("Session Closed");
-                    session.close(msg).await.unwrap()
-                }
-                _ => {
-                    println!("How did you get here")
-                }
-            }
-        }
+            };
+        };
+
+        websocket_server.disconnect(user.id as usize).await;
+        let _ = session
+            .close(Some(CloseReason {
+                code: CloseCode::Protocol,
+                description: Some(String::from(
+                    "Connection has been closed due to lack of heartbeat.",
+                )),
+            }))
+            .await;
+
+        log::info!(
+            "Websocket loop closed: UserId -> {}, Reason -> {:?}",
+            user.id,
+            close_reason
+        );
     });
 
     Ok(res)
 }
 
 async fn handle_json_request(
-    message: &str,
+    chat_server: &Arc<WebsocketServerHandler>,
+    message: &WebsocketMessage,
     user_id: i32,
     session: &mut actix_ws::Session,
     repository: &DatabaseConnection,
 ) {
-    let message: WebSocketMessage =
-        serde_json::from_str(message).expect("Unable to deserialize message");
+    let message = match message {
+        WebsocketMessage::Ping(_) => {
+            log::warn!("An ping message made it to handle_json_request");
+            return;
+        }
+        WebsocketMessage::Data(d) => d,
+    };
 
-    let output: Result<WebsocketResponseData, Box<dyn std::error::Error>> = match message.data {
+    let output: Result<WebsocketResponseData, Box<dyn std::error::Error>> = match message {
         WebsocketMessageData::Channel(_) => Ok(WebsocketResponseData::Ack("Ack".to_string())),
         WebsocketMessageData::Guild(_) => Ok(WebsocketResponseData::Ack("Ack".to_string())),
         WebsocketMessageData::Message(m) => handle_message(&m, user_id, repository)
             .await
             .map(WebsocketResponseData::Message),
         WebsocketMessageData::Ack(_) => Ok(WebsocketResponseData::Ack("Ack".to_string())),
+        _ => return,
     };
 
     match output {
         Ok(x) => {
-            session
-                .text(serde_json::to_string(&x).expect("Unable to stringify message"))
-                .await
-                .expect("unable to send message to client");
+            chat_server.send_message(x).await;
         }
         Err(e) => {
             error!("{:?}", e);
-            let response = WebSocketMessage {
-                data: WebsocketMessageData::Ack(String::from(
-                    "An error occurred while processing your request",
-                )),
-            };
+            let response = WebsocketMessage::Data(WebsocketMessageData::Ack(String::from(
+                "An error occurred while processing your request",
+            )));
 
             session
                 .text(serde_json::to_string(&response).expect("Unable to stringify message"))
